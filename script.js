5e00786cc5d0582f7657bfe2ee70a99869821bb0
vbuarque <viniciusbuarque17@gmail.com>
2023-10-23 18:21:29 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+// let firstName = 'Vinicius';
+// let lastName = 'Buarque';
+// let fullName = firstName + ' ' + lastName;
+// let email = 'viniciusbuarque17@gmail.com'
+// let age = 23;
+// let dateOfBirth = '31/05/2000';
+
+// console.log('Primeiro nome: ' + firstName);
+// console.log('Segundo nome: ' + lastName);
+// console.log('Nome completo: ' + fullName)
+// console.log('Email: ' + email)
+// console.log('Idade: ' + age);
+// console.log('Datade nascimento: ' + dateOfBirth);
+
+// ------------------------------------------------//
+
+// Variavel const não pode ser alterada, pois ela precisa ser constante, um valor na qual não pode ser alterado.
+const itemPrice = 30;
+
+// Se caso eu tentar modificar o valor da minha variavel const irá me retornar um erro: Uncaught TypeError: Assignment to constant variable.
+// itemPrice = 20; -> Aqui me retornara um erro
+console.log('Preço do item: ' + itemPrice);
